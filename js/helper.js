abbadfd37c7f4710aef78a6b1aa9bc0059711dbd
James McCarthy-Price <jmccarthy.price@gmail.com>
2016-01-12 23:28:58 +0800

    Add text to infoWindow

--- js/helper.js
+++ js/helper.js
@@ -172,7 +172,7 @@ function initializeMap() {
     // or hover over a pin on a map. They usually contain more information
     // about a location.
     var infoWindow = new google.maps.InfoWindow({
-      content: name
+      content: 'I have lived in ' + name
     });
 
     // hmmmm, I wonder what this is about...
